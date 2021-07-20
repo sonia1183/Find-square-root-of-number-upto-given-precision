bc49cdac8d7af816281ecfdecade0ddb824927ca
sonia1183 <80530004+sonia1183@users.noreply.github.com>
2021-07-20 09:51:04 +0530

    Update code.c

--- code.c
+++ code.c
@@ -1,19 +1,31 @@
+/*
+*Program which compute the square roor of a number without using any inbuilt function.
+*/
+
 #include<stdio.h>
+
+//Function to computer square root of the number.
 float square_root(int n)
 {
 	int s = 0, l = n;
 	int mid;
-	float res;
+	
+	//variable to store the answer
+	float res=0.0;
+	
 	while (s <= l) {
 		mid = (s + l) / 2;
+		//if its equal
 		if (mid * mid == n) {
 			res = mid;
 			break;
 		}
+		//for right side
 		if (mid * mid < n) {
 			s = mid + 1;
 			res = mid;
 		}
+		//for left side
 		else {
 			l = mid - 1;
 		}
@@ -22,15 +34,24 @@ float square_root(int n)
 	return res;
 }
 
-
+//Drive main function
 int main()
 {
-    int num;
-    scanf("%d",&num);
-    int p;
-    scanf("%d",&p);
-    float res=square_root(num);
-    float k = 0.1;
+        //Declare number and take value for number.
+        int num;
+        printf("Enter a number ");
+        scanf("%d",&num);
+    
+        //Declare precision and take value for precision.
+        int p;
+        printf("Enter a precision ");
+        scanf("%d",&p);
+	 
+	//Function calling 
+        float res=square_root(num);
+	
+	//For computing fractional part of square root upto given presion
+        float k = 0.1;
 	for (int i = 0; i < p; i++) {
 		while (res * res <= num) {
 			res += k;
@@ -39,6 +60,8 @@ int main()
 		res = res - k;
 		k = k / 10;
 	}
-    printf("%0.*f",p,res);
+	
+	//print the result
+        printf("%0.*f",p,res);
 	return 0;
 }
